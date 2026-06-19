942956b91e47bb05690e96684d84e9bb427a50c7
apptester <nzadiportfolio@gmail.com>
2026-06-19 16:14:39 +0200

    Index.html LightBox added
    Added lightbox image gallery | Page : Index.HTML

--- script.js
+++ script.js
@@ -70,3 +70,27 @@ if (contactForm) {
     }, 1500);
   });
 }
+
+// LIGHTBOX GALLERY
+
+const galleryImages = document.querySelectorAll(".gallery-img");
+
+const lightbox = document.getElementById("lightbox");
+
+const lightboxImg = document.getElementById("lightbox-img");
+
+const closeBtn = document.querySelector(".close");
+
+galleryImages.forEach((image) => {
+  image.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+
+    lightboxImg.src = image.src;
+  });
+});
+
+if (closeBtn) {
+  closeBtn.addEventListener("click", () => {
+    lightbox.style.display = "none";
+  });
+}
